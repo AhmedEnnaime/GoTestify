d01fed3347d5a97ae1a3481b93ebd5e961a63e51
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-07-13 20:47:23 +0100

    feat: :sparkles: added utils and tested create tweet func

--- repositories/tweet.go
+++ repositories/tweet.go
@@ -1,6 +1,8 @@
 package repositories
 
 import (
+	"errors"
+
 	"github.com/AhmedEnnaime/GoTestify/entities"
 	"github.com/jmoiron/sqlx"
 )
@@ -10,38 +12,83 @@ type tweetRepository struct {
 }
 
 type TweetRepository interface {
-	GetAllTweets() (*[]entities.Tweet, error)
-	GetTweetById(id int) (*entities.Tweet, error)
-	SearchTweetByText(text string) (*[]entities.Tweet, error)
+	// GetAllTweets() (*[]entities.Tweet, error)
 	CreateTweet(tweet *entities.Tweet) error
-	UpdateTweet(tweet *entities.Tweet) error
-	DeleteTweet(id int) error
+	// GetTweetById(id int) (*entities.Tweet, error)
+	// SearchTweetByText(text string) (*[]entities.Tweet, error)
+	// CreateTweet(tweet *entities.Tweet) error
+	// UpdateTweet(tweet *entities.Tweet) error
+	// DeleteTweet(id int) error
 }
 
 func InitializeTweetRepository(db *sqlx.DB) TweetRepository {
 	return &tweetRepository{db}
 }
 
-func (repo *tweetRepository) GetAllTweets() (*[]entities.Tweet, error) {
-	// Implementation for GetAllTweets
+// func (repository *tweetRepository) GetAllTweets() (*[]entities.Tweet, error) {
+// 	var result []entities.Tweet
+
+// 	rows, err := repository.db.Queryx(`SELECT id, username, text, created_at, modified_at FROM tweets`)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	for rows.Next() {
+// 		var tweet entities.Tweet
+// 		err = rows.StructScan(&tweet)
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 		result = append(result, tweet)
+// 	}
+// 	return &result, err
+// }
+
+// func (repository *tweetRepository) GetTweetById(id int) (*entities.Tweet, error) {
+// 	// Implementation for GetTweetById
+// }
+
+// func (repository *tweetRepository) SearchTweetByText(text string) (*[]entities.Tweet, error) {
+// 	// Implementation for SearchTweetByText
+// }
+
+func (repository *tweetRepository) CreateTweet(tweet *entities.Tweet) error {
+	var err error
+
+	if tweet == nil {
+		return errors.New("tweet can not be nil")
+	}
+	tx, err := repository.db.Beginx()
+	if err != nil {
+		return err
+	} else {
+		err = insertTweet(tx, tweet)
+		if err != nil {
+			return err
+		}
+	}
+
+	if err == nil {
+		tx.Commit()
+
+	} else {
+		tx.Rollback()
+	}
+
+	return err
+
 }
 
-func (repo *tweetRepository) GetTweetById(id int) (*entities.Tweet, error) {
-	// Implementation for GetTweetById
+func insertTweet(tx *sqlx.Tx, tweet *entities.Tweet) error {
+	_, err := tx.NamedExec(`INSERT INTO tweets(username, text)
+	VALUES (:username, :text);`, tweet)
+	return err
+
 }
 
-func (repo *tweetRepository) SearchTweetByText(text string) (*[]entities.Tweet, error) {
-	// Implementation for SearchTweetByText
-}
+// func (repository *tweetRepository) UpdateTweet(tweet *entities.Tweet) error {
+// 	// Implementation for UpdateTweet
+// }
 
-func (repo *tweetRepository) CreateTweet(tweet *entities.Tweet) error {
-	// Implementation for CreateTweet
-}
-
-func (repo *tweetRepository) UpdateTweet(tweet *entities.Tweet) error {
-	// Implementation for UpdateTweet
-}
-
-func (repo *tweetRepository) DeleteTweet(id int) error {
-	// Implementation for DeleteTweet
-}
+// func (repository *tweetRepository) DeleteTweet(id int) error {
+// 	// Implementation for DeleteTweet
+// }
--- repositories/tweet_test.go
+++ repositories/tweet_test.go
@@ -1,1 +1,38 @@
 package repositories
+
+import (
+	"github.com/AhmedEnnaime/GoTestify/config"
+	"github.com/AhmedEnnaime/GoTestify/entities"
+	"github.com/AhmedEnnaime/GoTestify/utils"
+	"github.com/stretchr/testify/suite"
+)
+
+type tweetRepositorySuite struct {
+	suite.Suite
+	repository      TweetRepository
+	cleanupExecutor utils.TruncateTableExecutor
+}
+
+func (suite *tweetRepositorySuite) SetupSuite() {
+	configs := config.GetConfig()
+	db := config.ConnectDB(configs)
+	repository := InitializeTweetRepository(db)
+
+	suite.repository = repository
+	suite.cleanupExecutor = utils.InitTruncateTableExecutor(db)
+
+}
+
+func (suite *tweetRepositorySuite) TearDownTest() {
+	defer suite.cleanupExecutor.TruncateTable([]string{"tweets"})
+}
+
+func (suite *tweetRepositorySuite) TestCreateTweet_Positive() {
+	tweet := entities.Tweet{
+		Username: "username",
+		Text:     "text",
+	}
+
+	err := suite.repository.CreateTweet(&tweet)
+	suite.NoError(err, "no error when creating tweet with valid input")
+}
